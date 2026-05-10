e949c7140258213cb10056b8f0d01d1a2a93ac5e
Pablo Isla <pablo.isla@tecsup.edu.pe>
2026-05-09 20:14:03 -0500

    add CRUD views and search filter for Vuelo

--- vuelos/views.py
+++ vuelos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Aerolinea, Vuelo
+from .serializers import AerolineaSerializer, VueloSerializer
 
-# Create your views here.
+
+class AerolineaViewSet(viewsets.ModelViewSet):
+    queryset = Aerolinea.objects.all()
+    serializer_class = AerolineaSerializer
+
+
+class VueloViewSet(viewsets.ModelViewSet):
+    queryset = Vuelo.objects.all()
+    serializer_class = VueloSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['codigo', 'origen', 'destino']
