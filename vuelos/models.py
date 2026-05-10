ce6b4c71b7859749ca53b75742e0897e9df6f402
Pablo Isla <pablo.isla@tecsup.edu.pe>
2026-05-09 20:09:57 -0500

    add models Aerolinea and Vuelo with relationship

--- vuelos/models.py
+++ vuelos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Aerolinea(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais_origen = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Vuelo(models.Model):
+    codigo = models.CharField(max_length=10)
+    origen = models.CharField(max_length=100)
+    destino = models.CharField(max_length=100)
+    aerolinea = models.ForeignKey(Aerolinea, on_delete=models.CASCADE, related_name='vuelos')
+
+    def __str__(self):
+        return self.codigo
